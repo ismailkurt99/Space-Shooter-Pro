d0e7b928390c7827cf938b14173146430bf1e508
İsmail Kurt <ismail.kurt7749@gmail.com>
2025-04-22 00:48:38 +0300

    Enemy Script, Laser and Player Updates

---
+++ Enemy.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{
+[SerializeField]
+private float _speed = 4f;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    transform.Translate(Vector3.down * _speed * Time.deltaTime);
+    if(transform.position.y < -6f)
+    {
+      float randomPosition = Random.Range(-8f, 8f);
+      transform.position = new Vector3(randomPosition, 8, 0);
+    }
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if(other.tag == "Player" )
+    {
+      Player player = other.transform.GetComponent<Player>();
+
+      if (player != null)
+      {
+        player.Damage();
+      }
+      
+      Destroy(this.gameObject);
+    }
+    else if (other.tag == "Laser")
+    {
+      Destroy(other.gameObject);
+      Destroy(this.gameObject);
+    }
+  }
+}
